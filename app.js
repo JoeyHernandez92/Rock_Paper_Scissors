57be80968797a6f891963dc2a6af3961d379edf4
JoeyHernandez92 <77796254+JoeyHernandez92@users.noreply.github.com>
2024-06-09 17:59:47 -0500

    working on buttons for rps

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = ['Rock','Paper','Scissors'];
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("clicked button");
+        playGame(button.getAttribute('choice'));
+    });
+});
+
 
 function getComputerChoice(){
-    const computerSelection = choices[Math.floor(Math.random() * 3)];
+    const computerSelection = choices[Math.floor(Math.random()*3)];
     return computerSelection;
-    
 }
+
+console.log(getComputerChoice());
+
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
     if(playerSelection === computerSelection){
-        return "It's a draw!";
+        return "It's a draw";
     }
-    else if(
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-    ){
-        return "You Win " + playerSelection + " beats " + computerSelection;
+    else if(playerSelection === 'rock' && computerSelection === 'scissors' ||
+            playerSelection === 'paper' && computerSelection ==='rock' ||
+            playerSelection === 'scissors' && computerSelection === 'paper'){
+                return "You Win!! " + playerSelection + " beats " + computerSelection;
     }
     else{
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+        return "You Lose " + computerSelection + " beats " + playerSelection;
     }
-}
+} 
 
-/*console.log("Computer picked " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));*/
-
-function playGame(){
+function playGame(playerSelection){
     for(let i = 0; i < 5; i++){
+        //const playerSelection = prompt("Please enter Rock,Paper,or Scissors");
         const computerSelection = getComputerChoice();
-        const playerSelection = prompt("Enter your choice!");
         console.log("Computer picked " + computerSelection);
+        console.log("You picked " + playerSelection);
         console.log(playRound(playerSelection, computerSelection));
     }
-    console.log("Thank you for playing!");
+    console.log("Thanks for playing");
 }
 
 playGame();
